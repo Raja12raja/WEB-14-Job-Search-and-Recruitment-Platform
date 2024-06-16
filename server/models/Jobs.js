52a786baf5041faf68c772afd0c268c9f1fce863
Naveen <146852997+NaveenSh7@users.noreply.github.com>
2024-06-17 00:42:05 +0530

    ..

--- server/models/Jobs.js
+++ server/models/Jobs.js
@@ -1,48 +1,4 @@
-// const mongoose =  require('mongoose');
 
-// const JobSchema = new mongoose.Schema({
-//     CompanyName: {
-//     type: String, 
-//     required:true,
-//   },
-//   email: {
-//     type: String, 
-//     required:true,
-//   },
-//   Role: {
-//     type: String, 
-//     required:true,
-//   },
-//   Skills: {
-//     type: String, 
-//     required:true,
-//   },
-//   Location: {
-//     type: String, 
-//     required:true,
-//   },
-//   Description: {
-//     type: String, 
-//     required:true,
-//   },
-//   Employmenttyp: {
-//     type: String, 
-//     required:true,
-//   },
-//   mSalary : {
-//     type: Number,
-//   },
-//   MSalary : {
-//     type: Number,
-//   },
-//   Deadline : {
-//     type: Date,
-//   },
-// });
-
-// // this adds data to colletion jobs
-// const JobModel = mongoose.model('Job', JobSchema);
-// module.exports = JobModel;
 
 
 const mongoose = require('mongoose');
