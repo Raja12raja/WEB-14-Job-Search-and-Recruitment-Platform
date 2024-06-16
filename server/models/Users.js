8b37eed5d92b9a5c084f1a021bae35958e7be69a
Naveen <146852997+NaveenSh7@users.noreply.github.com>
2024-06-17 00:44:27 +0530

    ..

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,21 +1,6 @@
 const mongoose =  require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  name: {
-    type: String, 
-    required:true,
-  },
-  email: {
-    type: String, 
-    required:true,
-  },
-  password: {
-    type: String, 
-    required:true,
-  },
-  age : {
-    type: Number,
-  },
 });
 
 // this adds data to colletion users
