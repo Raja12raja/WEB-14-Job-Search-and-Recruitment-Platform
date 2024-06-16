6ca90aff55ce999ab4ffba917142946440fbb831
Naveen <146852997+NaveenSh7@users.noreply.github.com>
2024-06-16 17:10:20 +0530

    changes in PostJob

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { Route, Routes, Router} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Contact from './components/Contact';
-import About from './components/About';
+import PostJob from './components/PostJob';
 import Profile from './components/Profile';
 import Login from './components/Login';
 
@@ -16,7 +16,7 @@ function App() {
     
       <Routes>
       <Route exact path="/" element={<Home />} />
-      <Route exact path="/about" element={<About />} />
+      <Route exact path="/PostJob" element={<PostJob />} />
       <Route exact path="/contact" element={<Contact />} />
       <Route exact path="/profile" element={<Profile />} />
       <Route exact path="/login" element={<Login />} />
