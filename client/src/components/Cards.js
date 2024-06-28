f0c2a2cd2b5628766be14d9ffad644370fc577dc
Sahil <sahilbharti259@gmail.com>
2024-06-28 11:41:56 +0530

    UI UPDATED AGAIN

--- client/src/components/Cards.js
+++ client/src/components/Cards.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Axios from 'axios';
 import location from './images/location.png';
-import nvidia from './images/nvidia.png';
-import google from './images/google.png';
 import ReadMore from "./ReadMore";
 
 const Cards = () => {
@@ -36,47 +34,45 @@ const Cards = () => {
     // Function to render cards
     const renderCards = () => {
         return allJobs.map((job, index) => (
-            <div key={index} className="w-full sm:w-1/2 md:w-1/2 lg:w-1/4 p-2 lg:p-4 mt-20">
-                <div className="w-72 overflow-hidden shadow-lg transition duration-500 ease-in-out transform hover:-translate-y-5 hover:shadow-2xl rounded-lg cursor-pointer bg-white mx-auto">
+            <div key={index} className="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 p-2 lg:p-3 mt-10">
+                <div className="w-80 overflow-hidden shadow-lg transition duration-500 ease-in-out transform hover:-translate-y-2 hover:shadow-2xl rounded-lg cursor-pointer bg-gradient-to-r from-white to-indigo-100 mx-auto border-l-4 border-r-4 border-indigo-500">
                     <a href="#" className="w-full block h-full">
-                        <div className="flex items-start">
-                            <div className="w-12 h-12 object-cover ml-4 mt-4">
+                        <div className="flex items-start p-4">
+                            <div className="w-16 h-16 object-cover">
                                 <img className="object-cover object-center w-full h-full rounded-lg" src={job.Logo} alt="Company Logo" />
                             </div>
                             <div className="ml-4">
-                                <div className="text-indigo-500 text-xl font-medium">{job.Role}</div>
-                                <div className="font-medium">@ {job.CompanyName}</div>
+                                <div className="text-indigo-600 text-xl font-semibold">{job.Role}</div>
+                                <div className="text-gray-700 font-medium">@ {job.CompanyName}</div>
                             </div>
                         </div>
-                        <div className="bg-white w-full p-4">
+                        <div className="bg-white w-full p-4 rounded-b-lg">
                             <div className="w-full flex flex-col items-start">
                                 <div className="w-full text-gray-800 text-sm font-medium mb-2">
-                                    <label>Skills :</label> <a>{job.Skills}</a>
+                                    <label>Skills :</label> <span>{job.Skills}</span>
                                 </div>
                                 <div className="w-full text-gray-800 text-sm font-medium mb-2">
-                                    <label>Salary :</label> <a>{job.mSalary}k - {job.MSalary}k</a>
+                                    <label>Salary :</label> <span>{job.mSalary}k - {job.MSalary}k</span>
                                 </div>
                                 <div className="w-full text-gray-800 text-sm font-medium mb-2">
-                                    <label>Deadline :</label> <a>{formatDate(job.Deadline)}</a>
+                                    <label>Deadline :</label> <span>{formatDate(job.Deadline)}</span>
                                 </div>
                                 <div className="w-full text-gray-800 text-sm font-medium mb-2 mt-2 min-h-14">
                                     <label className="text-indigo-700">Description :</label> <ReadMore text={job.Description} maxLength={50} />
                                 </div>
                             </div>
                             <div className="flex justify-between items-center py-3 border-b-2 text-xs text-white font-medium">
-                                <a href="/Apply" className="px-2 py-1 rounded bg-indigo-500 flex m-auto">
+                                <a href="/Apply" className="px-4 py-2 rounded-full bg-indigo-600 hover:bg-indigo-700 flex m-auto">
                                     Apply now
                                 </a>
                             </div>
                             <div className="flex items-center mt-4">
-                                <div className="px-3 bg-indigo-500 rounded text-white font-medium text-center">
+                                <div className="px-3 bg-indigo-600 rounded-full text-white font-medium text-center">
                                     {job.Employmenttype}
                                 </div>
-                                <div className="px-3 bg-indigo-500 rounded text-white font-medium text-center ml-auto">
-                                    <a className="flex items-center">
-                                        <img className="h-4 w-4 mt-1 mr-1" src={location} alt="Location Icon" />
-                                        {job.Location}
-                                    </a>
+                                <div className="px-3 bg-indigo-600 rounded-full text-white font-medium text-center ml-auto flex items-center">
+                                    <img className="h-4 w-4 mt-1 mr-1" src={location} alt="Location Icon" />
+                                    {job.Location}
                                 </div>
                             </div>
                         </div>
@@ -87,7 +83,7 @@ const Cards = () => {
     };
 
     return (
-        <div className="flex flex-wrap justify-center">
+        <div className="flex flex-wrap justify-center mx-4">
             {renderCards()}
             <div className="absolute bottom-0 right-0 mb-4 mr-4 z-10">
                 <div>
