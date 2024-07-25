1e9b65a3f26b0aa88e9424747655e2358bbf306b
dheeraj <raodheeraj532005@gmail.com>
2024-07-25 22:45:11 +0530

    fixed footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import './App.css';
-import { Router, Route, Routes, useLocation} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Dashboard from './components/Dashboard';
@@ -13,6 +12,7 @@ import Dashboard2 from './components/Dashboard2';
 import Manage from './components/Manage';
 import Footer1 from './components/Footer';
 import EditJob from './components/EditJob';
+import './App.css';
 
 function App() {
   const location = useLocation();
@@ -27,21 +27,23 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div className="App min-h-screen flex flex-col">
       {showNavbar()}
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/PostJob" element={<PostJob />} />
-        <Route exact path="/Dashboard" element={<Dashboard />} />
-        <Route path="/Dashboard2" element={<Dashboard2 />} />``
-        <Route exact path="/profile" element={<Profile />} />
-        <Route exact path="/login" element={<Login />} />
-        <Route path="/Apply/:id" element={<FillJob />} />
-        <Route exact path="/Manage/:id" element={<Manage />} />
-        <Route exact path="*" element={<Error404 />} />
-        <Route path="/edit/:id" element={<EditJob />} />
-      </Routes>
-     <Footer1 />
+      <div className="flex-grow">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/PostJob" element={<PostJob />} />
+          <Route exact path="/Dashboard" element={<Dashboard />} />
+          <Route path="/Dashboard2" element={<Dashboard2 />} />
+          <Route exact path="/profile" element={<Profile />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route path="/FillJob/:id" element={<FillJob />} />
+          <Route exact path="/Manage/:id" element={<Manage />} />
+          <Route exact path="*" element={<Error404 />} />
+          <Route path="/edit/:id" element={<EditJob />} />
+        </Routes>
+      </div>
+      <Footer1 />
     </div>
   );
 }
