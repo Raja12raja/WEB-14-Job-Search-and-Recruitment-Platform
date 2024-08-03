0e5ec84e6dde363c3defb8b630e8430c1b40d242
dheeraj <raodheeraj532005@gmail.com>
2024-08-04 01:37:23 +0530

    some ui updat

--- client/src/components/Manage.js
+++ client/src/components/Manage.js
@@ -61,24 +61,38 @@ const MDashboard = () => {
     }
   };
 
-    if (isLoading) {
-        return <div className="text-center mt-10"> <div className="text-center "><div class="flex items-center justify-center w-full h-[100vh] text-gray-900 ">
-
-      
-        <div class="flex items-center justify-center w-full h-[100vh] text-gray-900 transform scale-50 mb-40">
-          <div>
-            <h1 class="text-xl md:text-7xl font-bold flex items-center">L
-              <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" class="animate-spin" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2ZM13.6695 15.9999H10.3295L8.95053 17.8969L9.5044 19.6031C10.2897 19.8607 11.1286 20 12 20C12.8714 20 13.7103 19.8607 14.4956 19.6031L15.0485 17.8969L13.6695 15.9999ZM5.29354 10.8719L4.00222 11.8095L4 12C4 13.7297 4.54894 15.3312 5.4821 16.6397L7.39254 16.6399L8.71453 14.8199L7.68654 11.6499L5.29354 10.8719ZM18.7055 10.8719L16.3125 11.6499L15.2845 14.8199L16.6065 16.6399L18.5179 16.6397C19.4511 15.3312 20 13.7297 20 12L19.997 11.81L18.7055 10.8719ZM12 9.536L9.656 11.238L10.552 14H13.447L14.343 11.238L12 9.536ZM14.2914 4.33299L12.9995 5.27293V7.78993L15.6935 9.74693L17.9325 9.01993L18.4867 7.3168C17.467 5.90685 15.9988 4.84254 14.2914 4.33299ZM9.70757 4.33329C8.00021 4.84307 6.53216 5.90762 5.51261 7.31778L6.06653 9.01993L8.30554 9.74693L10.9995 7.78993V5.27293L9.70757 4.33329Z">
-                </path>
-              </svg> ading . . .
-            </h1>
+  if (isLoading) {
+    return (
+      <div className="text-center mt-10">
+        {" "}
+        <div className="text-center ">
+          <div class="flex items-center justify-center w-full h-[100vh] text-gray-900 ">
+            <div class="flex items-center justify-center w-full h-[100vh] text-gray-900 transform scale-50 mb-40">
+              <div>
+                <h1 class="text-xl md:text-7xl font-bold flex items-center">
+                  L
+                  <svg
+                    stroke="currentColor"
+                    fill="currentColor"
+                    stroke-width="0"
+                    viewBox="0 0 24 24"
+                    class="animate-spin"
+                    height="1em"
+                    width="1em"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2ZM13.6695 15.9999H10.3295L8.95053 17.8969L9.5044 19.6031C10.2897 19.8607 11.1286 20 12 20C12.8714 20 13.7103 19.8607 14.4956 19.6031L15.0485 17.8969L13.6695 15.9999ZM5.29354 10.8719L4.00222 11.8095L4 12C4 13.7297 4.54894 15.3312 5.4821 16.6397L7.39254 16.6399L8.71453 14.8199L7.68654 11.6499L5.29354 10.8719ZM18.7055 10.8719L16.3125 11.6499L15.2845 14.8199L16.6065 16.6399L18.5179 16.6397C19.4511 15.3312 20 13.7297 20 12L19.997 11.81L18.7055 10.8719ZM12 9.536L9.656 11.238L10.552 14H13.447L14.343 11.238L12 9.536ZM14.2914 4.33299L12.9995 5.27293V7.78993L15.6935 9.74693L17.9325 9.01993L18.4867 7.3168C17.467 5.90685 15.9988 4.84254 14.2914 4.33299ZM9.70757 4.33329C8.00021 4.84307 6.53216 5.90762 5.51261 7.31778L6.06653 9.01993L8.30554 9.74693L10.9995 7.78993V5.27293L9.70757 4.33329Z"></path>
+                  </svg>{" "}
+                  ading . . .
+                </h1>
+              </div>
+            </div>
           </div>
         </div>
-        
-          </div></div>;</div>;
-    }
+        ;
+      </div>
+    );
+  }
 
   if (!isAuthenticated) {
     return (
@@ -90,7 +104,7 @@ const MDashboard = () => {
 
   return (
     <div className="p-4 bg-gray-100 min-h-screen">
-        <div className=" bg-gray-300 text-xl p-4 rounded-lg shadow">
+      <div className=" bg-gray-300 text-xl p-4 rounded-lg shadow hover:bg-gray-400 transition-colors duration-300">
         Pending Applications
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-10 mt-4 w-full">
@@ -104,7 +118,12 @@ const MDashboard = () => {
               className="flex items-center relative p-6 w-full bg-white rounded-lg overflow-hidden shadow hover:shadow-lg transition-shadow duration-300"
               key={index}
             >
-              <div className="w-16 h-16 rounded-full bg-gray-100"><img src="https://static.vecteezy.com/system/resources/previews/000/662/785/original/man-face-cartoon-vector.jpg" alt="profil" /></div>
+              <div className="w-16 h-16 rounded-full bg-gray-100">
+                <img
+                  src="https://static.vecteezy.com/system/resources/previews/000/662/785/original/man-face-cartoon-vector.jpg"
+                  alt="profil"
+                />
+              </div>
               <div className="ml-4">
                 <p className="font-bold text-gray-800 text-lg">
                   {row.UserName}
@@ -148,7 +167,7 @@ const MDashboard = () => {
         )}
       </div>
 
-      <div className="mt-10 bg-green-500 text-xl p-4 rounded-lg shadow">
+      <div className=" mx-auto mt-10 bg-green-500 text-xl p-4 rounded-lg shadow hover:bg-green-600 transition-colors duration-300">
         Accepted Applications
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 mt-4 w-full">
@@ -162,7 +181,12 @@ const MDashboard = () => {
               className="flex items-center relative p-6 w-full bg-white text-black rounded-lg overflow-hidden shadow hover:shadow-lg transition-shadow duration-300"
               key={index2}
             >
-              <div className="w-16 h-16 rounded-full bg-gray-100"><img src="https://static.vecteezy.com/system/resources/previews/000/662/785/original/man-face-cartoon-vector.jpg" alt="profile pic" /></div>
+              <div className="w-16 h-16 rounded-full bg-gray-100">
+                <img
+                  src="https://static.vecteezy.com/system/resources/previews/000/662/785/original/man-face-cartoon-vector.jpg"
+                  alt="profile pic"
+                />
+              </div>
               <div className="ml-4">
                 <p className="font-bold text-lg">{row2.UserName}</p>
                 <p className="text-sm">
@@ -196,7 +220,7 @@ const MDashboard = () => {
         )}
       </div>
 
-      <div className="mt-10 bg-red-400 text-xl p-4 rounded-lg shadow">
+      <div className="  mx-auto mt-10 bg-red-400 text-xl p-4 rounded-lg shadow hover:bg-red-500 transition-colors duration-300">
         Declined Applications
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 mt-4 w-full">
@@ -210,7 +234,12 @@ const MDashboard = () => {
               className="flex items-center relative p-6 w-full bg-white text-black rounded-lg overflow-hidden shadow hover:shadow-lg transition-shadow duration-300"
               key={index3}
             >
-              <div className="w-16 h-16 rounded-full bg-gray-100"><img src="https://static.vecteezy.com/system/resources/previews/000/662/785/original/man-face-cartoon-vector.jpg" alt="profile pic" /></div>
+              <div className="w-16 h-16 rounded-full bg-gray-100">
+                <img
+                  src="https://static.vecteezy.com/system/resources/previews/000/662/785/original/man-face-cartoon-vector.jpg"
+                  alt="profile pic"
+                />
+              </div>
               <div className="ml-4">
                 <p className="font-bold text-lg">{row3.UserName}</p>
                 <p className="text-sm">
